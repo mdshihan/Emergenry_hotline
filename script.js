07d6f061c2357e2b3194620ee8559930721b0ef4
shihan.cse24@gmail.com <shihan.cse24@gmail.com>
2025-08-28 21:33:01 +0600

    add call fucntionality with js

--- script.js
+++ script.js
@@ -8,10 +8,28 @@ for(const heartcnt of heartCount ){
     })
 }
 
-const clickCall= document.getElementsByClassName("call-btn")
-for(const call of clickCall){
-    call.addEventListener('click', function(e){
-        e.preventDefault();
-        alert("")
-    })
+// call section 
+
+const callButtons = document.getElementsByClassName("call-btn");
+const callRate = document.getElementById("rate")
+
+for (const btn of callButtons) {
+  btn.addEventListener("click", function () {
+    const card = btn.closest("div.bg-white");
+    let  available = parseInt(callRate.innerText);
+    if(available<20){
+        alert("You have not sufficiant coin. Please recharge!")
+        return
+    }
+
+    const serviceName = card.querySelector("div.my-6 h1").innerText;
+
+    const serviceNumber = card.querySelector("div.my-6 + h1").innerText;
+
+    alert(` Calling ${serviceName} ${serviceNumber}`);
+    let afterCallBalance = available-20;
+    callRate.innerText= afterCallBalance
+  });
 }
+
+
